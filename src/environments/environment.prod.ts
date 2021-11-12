69581be1457ae5cde8466be70e4eef2bf4aa3ca0
Maria Paula <maria.paula.mosquera@correounivalle.edu.co>
2021-11-12 07:47:52 -0500

    Add improvements to the css styles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  url_api: 'https://rickandmortyapi.com/api',
+  firebase: {
+    apiKey: "AIzaSyBAY0ILpb-dk40VaZc276Lztl1unDnN3rE",
+    authDomain: "callmemapa-rickandmorty.firebaseapp.com",
+    projectId: "callmemapa-rickandmorty",
+    storageBucket: "callmemapa-rickandmorty.appspot.com",
+    messagingSenderId: "644961981597",
+    appId: "1:644961981597:web:761bbe750ebe4aadf63074",
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-  url_api: 'https://rickandmortyapi.com/api'
+  url_api: 'https://rickandmortyapi.com/api',
+  firebase: {
+    apiKey: "AIzaSyBAY0ILpb-dk40VaZc276Lztl1unDnN3rE",
+    authDomain: "callmemapa-rickandmorty.firebaseapp.com",
+    projectId: "callmemapa-rickandmorty",
+    storageBucket: "callmemapa-rickandmorty.appspot.com",
+    messagingSenderId: "644961981597",
+    appId: "1:644961981597:web:761bbe750ebe4aadf63074",
+  }
 };
 
 /*
